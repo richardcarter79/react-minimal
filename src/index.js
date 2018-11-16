517da2737280f7921aa110b8e90de3b0b460d9a7
Dikvega <riggz.79@gmail.com>
2018-11-16 08:47:28 -0600

    added React and Hot module replacement to webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('My minimal react webpack babel setup');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const title = 'My Minimal React Webpack Babel Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
