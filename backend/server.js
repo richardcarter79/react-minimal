d7df8d28bec9a4872d8bddfdf734e1fd53feb7ad
Dikvega <riggz.79@gmail.com>
2019-09-03 09:20:12 -0500

    making some changes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
-// const path = require('path');
 const os = require('os');
-
 const app = express();
-
 
 const port = 8080;
 //  middleware to define folder for static files
